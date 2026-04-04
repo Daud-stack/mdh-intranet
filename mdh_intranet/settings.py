3d82e1a22ce764674505f41daf5b5d74e7f1eb9a
Daud Sibanda <58472949+Daud-stack@users.noreply.github.com>
2026-04-04 16:04:11 +0200

    Enable debug mode for deletion troubleshooting

--- mdh_intranet/settings.py
+++ mdh_intranet/settings.py
@@ -33,7 +33,8 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
+DEBUG = True
+# DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
 # In production, set DJANGO_ALLOWED_HOSTS to your domain(s)
 ALLOWED_HOSTS = os.environ.get(
